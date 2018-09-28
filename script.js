7796720c7c0c7bb28b92a2c00219ad47bddfafee
LiveG <hi@liveg.tech>
2018-09-28 10:27:45 +0100

    Add email verification

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 $(function() {
     $("input[type='email']").each(function() {
         $(this).next().children().css({
@@ -11,15 +16,9 @@ $(function() {
                 "padding-left": "0px",
                 "width": "300px"
             });
+        });
 
-            // $(this).css({
-            //     "padding-right": "0px",
-            //     "width": "300px"
-            // });
-        })
-
-        $(this).blur(function() {
-            
+        $(this).blur(function() {      
             if ($(this).val() == "") {
                 $(this).next().children().css({
                     "padding-left": "315px",
@@ -31,6 +30,14 @@ $(function() {
                     "width": "300px"
                 });
             }
-        })
+        });
+
+        $(this).on("input", function() {      
+            if ($(this).val() != "" && !validateEmail($(this).val() + "@norwich-school.org.uk")) {
+                $(this).css("color", "red");
+            } else {
+                $(this).css("color", "black");
+            }
+        });
     });
 });
